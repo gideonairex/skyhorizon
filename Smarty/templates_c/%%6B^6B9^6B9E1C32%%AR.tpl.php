ccf1922778fc656b9f6322e220aa041650e0408f
gideonaireX <gideonrosales@gmail.com>
2014-08-18 22:53:44 +0800

    Fix collection

--- Smarty/templates_c/%%6B^6B9^6B9E1C32%%AR.tpl.php
+++
@@ -1,4 +0,0 @@
-<?php /* Smarty version 2.6.18, created on 2014-07-29 21:43:00
-         compiled from ReportTemplates/AR.tpl */ ?>
-<link type="text/css" rel="stylesheet" href="include/components/bootstrap/dist/css/bootstrap.css" />
-HELLo
