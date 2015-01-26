514dd74af184a48a40576ff59b9e11b19e4fac92
Gideon Rosales <gideonrosales@gmail.com>
2015-01-27 07:10:43 +0800

    Collection adjustment

---
+++ Smarty/templates/ReportTemplates/footercollection.tpl
@@ -0,0 +1,22 @@
+<div class="col-xs-12" style="margin-top:20px">
+
+	<div class="col-xs-12">
+		<div class="col-xs-2">
+			<p><strong>Approved by:</strong></p>
+		</div>
+		<div class="col-xs-3">
+			<p class="text-center" style="border-bottom:1px solid black">Johann Estrada</p>
+			<p class="text-center"><strong>General Manager</strong></p>
+		</div>
+		<div class="col-xs-2"></div>
+
+		<div class="col-xs-2">
+			<p><strong>Reviewed by:</strong></p>
+		</div>
+		<div class="col-xs-3">
+			<p class="text-center" style="border-bottom:1px solid black">&nbsp;&nbsp;&nbsp;&nbsp;</p>
+		</div>
+		<div class="col-xs-2"></div>
+	</div>
+
+</div>
--- modules/SHReports/Reports/collection.php
+++ modules/SHReports/Reports/collection.php
@@ -21,11 +21,16 @@
 		$ext .= "and currency_cl = '".$_REQUEST['conversion']."' ";
 	}
 
-	$query = "select * from vtiger_collectionlogs
+	$query = "select *, vtiger_collection.collection_no as `collection_no_`, 
+		vtiger_collectionlogs.amount as `amount_`,
+		vtiger_collectionlogs.ewt as `ewt_`,
+		vtiger_collectionlogs.bc as `bc_`
+		from vtiger_collectionlogs
 		inner join vtiger_crmentity on vtiger_collectionlogs.collectionlogsid = vtiger_crmentity.crmid
 		inner join vtiger_collection on vtiger_collectionlogs.collection_no = vtiger_collection.collectionid
 		inner join vtiger_accountsreceivable on vtiger_accountsreceivable.accountsreceivableid = vtiger_collectionlogs.ar_no
 		inner join vtiger_salesagreement on vtiger_salesagreement.salesagreementid = vtiger_accountsreceivable.sales_no
+		left join vtiger_archecks on vtiger_collection.collectionid = vtiger_archecks.collection_no
 						where deleted = 0 ".$ext;
 
 	$result = $adb->pquery($query,array());
@@ -37,27 +42,32 @@
 		for( $i = 0 ; $i < $num_rows; $i++){
 			$method = explode( ' ',$adb->query_result($result, $i, "c_payment_method"));
 			$method = implode( '_', $method);
-			$data['summary'][$method]['payment'] += $adb->query_result($result, $i, "amount");
-			$data['summary'][$method]['awt'] += $adb->query_result($result, $i, "ewt");
-			$data['summary'][$method]['bc'] += $adb->query_result($result, $i, "bc");
-			$data['summary'][$method]['total'] += $adb->query_result($result, $i, "bc") + $adb->query_result($result, $i, "amount") + $adb->query_result($result, $i, "ewt");
+			$data['summary'][$method]['payment'] += $adb->query_result($result, $i, "amount_");
+			$data['summary'][$method]['awt'] += $adb->query_result($result, $i, "ewt_");
+			$data['summary'][$method]['bc'] += $adb->query_result($result, $i, "bc_");
+			$data['summary'][$method]['total'] += $adb->query_result($result, $i, "bc_") + $adb->query_result($result, $i, "amount_") + $adb->query_result($result, $i, "ewt_");
 			$data['summary'][$method]['c_payment_method'] = $method;
-			$collection_no = $adb->query_result($result, $i, "collection_no");
-			$data['summary'][$method]['details'][$collection_no][$i]['payment'] = $adb->query_result($result, $i, "amount");
-			$data['summary'][$method]['details'][$collection_no][$i]['c_payment_method'] = $adb->query_result($result, $i, "c_payment_method");
-			$data['summary'][$method]['details'][$collection_no][$i]['awt'] = $adb->query_result($result, $i, "ewt");
-			$data['summary'][$method]['details'][$collection_no][$i]['bc'] = $adb->query_result($result, $i, "bc");
-			$data['summary'][$method]['details'][$collection_no][$i]['total'] =  $adb->query_result($result, $i, "bc") + $adb->query_result($result, $i, "amount") + $adb->query_result($result, $i, "ewt");
-			$data['summary'][$method]['details'][$collection_no][$i]['ar_no'] = "<a href='index.php?module=AccountsReceivable&action=DetailView&record=".$adb->query_result($result, $i, "accountsreceivableid")."' target=_blank >".$adb->query_result($result, $i, "ar_no")."</a>";
-			$data['summary'][$method]['details'][$collection_no][$i]['sa_no'] = "<a href='index.php?module=SalesAgreement&action=DetailView&record=".$adb->query_result($result, $i, "salesagreementid")."' target=_blank >".$adb->query_result($result, $i, "sa_no")."</a>";
-			$data['summary'][$method]['details'][$collection_no][$i]['ar_no_'] = $adb->query_result($result, $i, "ar_no");
-			$data['summary'][$method]['details'][$collection_no][$i]['sa_no_'] = $adb->query_result($result, $i, "sa_no");
-			$data['summary'][$method]['details'][$collection_no][$i]['cl_no'] = "<a href='index.php?module=CollectionLogs&action=DetailView&record=".$adb->query_result($result, $i, "collectionlogsid")."' target=_blank >".$adb->query_result($result, $i, "collection_log_no")."</a>";
-			$data['summary'][$method]['details'][$collection_no][$i]['cl_no_'] = $adb->query_result($result, $i, "collection_log_no");
-			$data['summary'][$method]['details'][$collection_no][$i]['sales'] = $adb->query_result($result, $i, "sales");
-			$data['summary']['Summary']['payment'] += $adb->query_result($result, $i, "amount");
-			$data['summary']['Summary']['awt'] += $adb->query_result($result, $i, "ewt");
-			$data['summary']['Summary']['bc'] += $adb->query_result($result, $i, "bc");
+			$collection_no = $adb->query_result($result, $i, "collection_no_");
+
+			$data['summary'][$method]['details'][$collection_no]['detail']['chk_no'] = $adb->query_result($result, $i, "chk_no");
+			$data['summary'][$method]['details'][$collection_no]['detail']['bank'] = $adb->query_result($result, $i, "bank");
+			$data['summary'][$method]['details'][$collection_no]['detail']['date_of_chk'] = date("F j, Y", strtotime( $adb->query_result($result, $i, "date_of_chk") ) );
+			$data['summary'][$method]['details'][$collection_no]['detail']['arhk_status'] = $adb->query_result($result, $i, "arhk_status");
+			$data['summary'][$method]['details'][$collection_no]['lists'][$i]['payment'] = $adb->query_result($result, $i, "amount_");
+			$data['summary'][$method]['details'][$collection_no]['lists'][$i]['c_payment_method'] = $adb->query_result($result, $i, "c_payment_method");
+			$data['summary'][$method]['details'][$collection_no]['lists'][$i]['awt'] = $adb->query_result($result, $i, "ewt_");
+			$data['summary'][$method]['details'][$collection_no]['lists'][$i]['bc'] = $adb->query_result($result, $i, "bc_");
+			$data['summary'][$method]['details'][$collection_no]['lists'][$i]['total'] =  $adb->query_result($result, $i, "bc_") + $adb->query_result($result, $i, "amount_") + $adb->query_result($result, $i, "ewt_");
+			$data['summary'][$method]['details'][$collection_no]['lists'][$i]['ar_no'] = "<a href='index.php?module=AccountsReceivable&action=DetailView&record=".$adb->query_result($result, $i, "accountsreceivableid")."' target=_blank >".$adb->query_result($result, $i, "ar_no")."</a>";
+			$data['summary'][$method]['details'][$collection_no]['lists'][$i]['sa_no'] = "<a href='index.php?module=SalesAgreement&action=DetailView&record=".$adb->query_result($result, $i, "salesagreementid")."' target=_blank >".$adb->query_result($result, $i, "sa_no")."</a>";
+			$data['summary'][$method]['details'][$collection_no]['lists'][$i]['ar_no_'] = $adb->query_result($result, $i, "ar_no");
+			$data['summary'][$method]['details'][$collection_no]['lists'][$i]['sa_no_'] = $adb->query_result($result, $i, "sa_no");
+			$data['summary'][$method]['details'][$collection_no]['lists'][$i]['cl_no'] = "<a href='index.php?module=CollectionLogs&action=DetailView&record=".$adb->query_result($result, $i, "collectionlogsid")."' target=_blank >".$adb->query_result($result, $i, "collection_log_no")."</a>";
+			$data['summary'][$method]['details'][$collection_no]['lists'][$i]['cl_no_'] = $adb->query_result($result, $i, "collection_log_no");
+			$data['summary'][$method]['details'][$collection_no]['lists'][$i]['sales'] = $adb->query_result($result, $i, "sales");
+			$data['summary']['Summary']['payment'] += $adb->query_result($result, $i, "amount_");
+			$data['summary']['Summary']['awt'] += $adb->query_result($result, $i, "ewt_");
+			$data['summary']['Summary']['bc'] += $adb->query_result($result, $i, "bc_");
 		}
 	}
 
