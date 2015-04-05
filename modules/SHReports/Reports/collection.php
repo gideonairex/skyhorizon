a9fc3d47c278171312ddccc140939a43230aacfa
Gideon Rosales <gideonrosales@gmail.com>
2015-04-05 19:12:30 +0800

    fix: collection report

--- modules/SHReports/Reports/collection.php
+++ modules/SHReports/Reports/collection.php
@@ -29,7 +29,8 @@
 		inner join vtiger_crmentity on vtiger_collectionlogs.collectionlogsid = vtiger_crmentity.crmid
 		inner join vtiger_collection on vtiger_collectionlogs.collection_no = vtiger_collection.collectionid
 		inner join vtiger_accountsreceivable on vtiger_accountsreceivable.accountsreceivableid = vtiger_collectionlogs.ar_no
-		inner join vtiger_salesagreement on vtiger_salesagreement.salesagreementid = vtiger_accountsreceivable.sales_no
+		left join vtiger_salesagreement on vtiger_salesagreement.salesagreementid = vtiger_accountsreceivable.sales_no
+		left join vtiger_nontrader on vtiger_nontrader.nontraderid = vtiger_accountsreceivable.sales_no
 		left join vtiger_archecks on vtiger_collection.collectionid = vtiger_archecks.collection_no
 						where deleted = 0 ".$ext;
 
@@ -59,9 +60,16 @@
 			$data['summary'][$method]['details'][$collection_no]['lists'][$i]['bc'] = $adb->query_result($result, $i, "bc_");
 			$data['summary'][$method]['details'][$collection_no]['lists'][$i]['total'] =  $adb->query_result($result, $i, "bc_") + $adb->query_result($result, $i, "amount_") + $adb->query_result($result, $i, "ewt_");
 			$data['summary'][$method]['details'][$collection_no]['lists'][$i]['ar_no'] = "<a href='index.php?module=AccountsReceivable&action=DetailView&record=".$adb->query_result($result, $i, "accountsreceivableid")."' target=_blank >".$adb->query_result($result, $i, "ar_no")."</a>";
-			$data['summary'][$method]['details'][$collection_no]['lists'][$i]['sa_no'] = "<a href='index.php?module=SalesAgreement&action=DetailView&record=".$adb->query_result($result, $i, "salesagreementid")."' target=_blank >".$adb->query_result($result, $i, "sa_no")."</a>";
+
+			if( $adb->query_result( $result, $i, "nontraderid" ) == '' ) {
+				$data['summary'][$method]['details'][$collection_no]['lists'][$i]['sa_no'] = "<a href='index.php?module=SalesAgreement&action=DetailView&record=".$adb->query_result($result, $i, "salesagreementid")."' target=_blank >".$adb->query_result($result, $i, "sa_no")."</a>";
+				$data['summary'][$method]['details'][$collection_no]['lists'][$i]['sa_no_'] = $adb->query_result($result, $i, "sa_no");
+			} else {
+				$data['summary'][$method]['details'][$collection_no]['lists'][$i]['sa_no'] = "<a href='index.php?module=NonTradeR&action=DetailView&record=".$adb->query_result($result, $i, "nontraderid")."' target=_blank >".$adb->query_result($result, $i, "ntr_no")."</a>";
+				$data['summary'][$method]['details'][$collection_no]['lists'][$i]['sa_no_'] = $adb->query_result($result, $i, "ntr_no");
+			}
+
 			$data['summary'][$method]['details'][$collection_no]['lists'][$i]['ar_no_'] = $adb->query_result($result, $i, "ar_no");
-			$data['summary'][$method]['details'][$collection_no]['lists'][$i]['sa_no_'] = $adb->query_result($result, $i, "sa_no");
 			$data['summary'][$method]['details'][$collection_no]['lists'][$i]['cl_no'] = "<a href='index.php?module=CollectionLogs&action=DetailView&record=".$adb->query_result($result, $i, "collectionlogsid")."' target=_blank >".$adb->query_result($result, $i, "collection_log_no")."</a>";
 			$data['summary'][$method]['details'][$collection_no]['lists'][$i]['cl_no_'] = $adb->query_result($result, $i, "collection_log_no");
 			$data['summary'][$method]['details'][$collection_no]['lists'][$i]['sales'] = $adb->query_result($result, $i, "sales");
