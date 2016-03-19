d15963b1a84c2115c8a9ae3ffd249e44c7bab2e9
root <root@ip-172-31-27-91.ap-southeast-1.compute.internal>
2016-03-19 10:10:11 +0000

    cleanup

--- Smarty/templates_c/%%7E^7ED^7ED4E1A1%%logo.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.18, created on 2014-11-04 16:48:17
-         compiled from ReportTemplates/logo.tpl */ ?>
-<img src="Smarty/misc/skyhorizon.jpg" class="img-responsive" alt="Responsive image">
-<address>
-	<p class="text-center"> <strong> <small> Calbayog branch </small> </strong></p>
-	<p class="text-center"> <strong> <small> Maharlika Highway Brgy. Obrero, Calbyog City </small></strong></p>
-	<p class="text-center"> <strong> <small> Tel No. ( 055 ) - 2091118 , ( 055 ) - 5339747 </small> </strong></p>
-	<p class="text-center"> <strong> <small> Email: skyhorizon_calbayog@yahoo.com </small> </strong></p>
-</address>
--- Smarty/templates_c/%%E4^E4D^E4DAC3FF%%LoginFooter.tpl.php
+++
@@ -1,4 +0,0 @@
-<?php /* Smarty version 2.6.18, created on 2014-09-25 13:14:36
-         compiled from LoginFooter.tpl */ ?>
-	</div>
-</body>
